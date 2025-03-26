3e2cecad2450a8f7f0106f3fea60ea406e4a6681
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2025-03-26 01:11:06 +0100

    Add load_faiss.py to initialize vectorstore with new text files

---
+++ src/utilities/load_faiss.py
@@ -0,0 +1,8 @@
+from src.utilities.vectorstore_builder import build_faiss_vectorstore
+
+# You can add new text files here to include them in the vectorstore.
+# This will allow the model to reference the new information.
+# You can always change the file directory if the file is not in the project directory.
+build_faiss_vectorstore(
+    ["docs/kb.txt", "docs/meta_guidance.txt"]  # <— this is the new addition
+)
