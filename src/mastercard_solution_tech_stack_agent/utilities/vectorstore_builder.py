e125f4097342910c9b6825bde04dc6099b6a5078
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2025-05-16 09:14:15 +0100

    refactor: Clean up and organize vectorstore_builder.py for improved readability

--- src/mastercard_solution_tech_stack_agent/utilities/vectorstore_builder.py
+++ src/mastercard_solution_tech_stack_agent/utilities/vectorstore_builder.py
@@ -1,6 +1,8 @@
+import os
 from typing import List
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 
